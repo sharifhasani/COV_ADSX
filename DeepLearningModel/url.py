ed42e45f999576a788783d9db5270073559fb096
sharifhasani <sharif.bashoma@gmail.com>
2021-09-23 23:57:16 +0330

    add template

---
+++ DeepLearningModel/url.py
@@ -0,0 +1,5 @@
+from django.urls import path
+from .views import indexView
+urlpatterns = [
+    path("", indexView.as_view())
+]
--- DeepLearningModel/views.py
+++ DeepLearningModel/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views import View
 
+class indexView(View):
+    def get(self, request, *args, **kwargs):
+        return render(request, "index.html")
+
+    def post(self, request, *args, **kwargs):
+        return HttpResponse('POST request!')
 # Create your views here.
